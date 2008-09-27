b4989311f754f876c85534e0bc29d7c1d03c1085
kevinfjbecker <kevinfjbecker@96f814b5-8e46-0410-8938-e1327941a63d>
2008-09-27 22:24:06 +0000

    Added '{' to the .toString for card.COneShotCard

--- MunchkinSimulator/src/card/COneShotCard.java
+++ MunchkinSimulator/src/card/COneShotCard.java
@@ -3,7 +3,7 @@ package card;
 public class COneShotCard extends CItemCard {
 
 	public String toString() {
-		String s = "SimpleOneShotCard";
+		String s = "SimpleOneShotCard{";
 		s += "bonus:" + this.bonus + ", ";
 		s += "value:" + this.value + "}";
 		return (s);
